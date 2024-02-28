bc099dc5da2eaa8998016cc1295c97d4cc410208
nolan7512 <75007954+nolan7512@users.noreply.github.com>
2024-02-28 19:26:35 +0700

    add header

---
+++ unit/header.sh
@@ -0,0 +1,32 @@
+GREEN="\e[1m\e[1;32m" # green color
+RED="\e[1m\e[1;31m" # red color
+BLUE='\033[0;34m'   # blue color
+NC="\e[0m"          # no color
+
+function printLogo {
+  bash <(curl -s https://raw.githubusercontent.com/nolan7512/automatic-install-namada/main/unit/adamanlogo.sh)
+}
+
+function printLine {
+  echo "------------------------------------------------------------------------------------"
+}
+
+function printGreen {
+  echo -e "${GREEN}${1}${NC}"
+}
+
+function printRed {
+  echo -e "${RED}${1}${NC}"
+}
+
+function printBlue {
+  echo -e "${BLUE}${1}${NC}"
+}
+
+function addToPath {
+  source $HOME/.bash_profile
+  PATH_EXIST=$(grep "${1}" $HOME/.bash_profile)
+  if [ -z "$PATH_EXIST" ]; then
+    echo "export PATH=\$PATH:${1}" >> $HOME/.bash_profile
+  fi
+}
